b18e3dc713f9cc73f33f970601132563460be78c
Benjamin Solum <benjamin@soluml.com>
2018-04-09 21:55:58 -0500

    Update

--- src/client/js/components/root.tsx
+++ src/client/js/components/root.tsx
@@ -1,28 +1,12 @@
 import React from 'react';
 
-/*
-import Header from '../containers/header';
-import Tabs from '../containers/tabs';
-import Nav from '../containers/nav';
-import Modules from '../containers/modules';
-import User from '../containers/user';
-import { ROUTES_USER } from '../constants';
-
 const attrs = {
   className: styled`
-    background-color: $navBgColor;
-    display: grid;
-    grid-template-columns: calc(#{$mobileHitBox} + #{$rutter}) repeat(4, 1fr);
-    grid-template-rows: auto auto 1fr;
-    height: 100vh;
-    left: 0;
-    position: absolute;
-    top: 0;
-    width: 100vw;
+    background-color: navy;
+    color: white;
   `
 };
-*/
 
-export default () => (<div>
+export default () => (<div {...attrs}>
   Root!!!?
 </div>);
